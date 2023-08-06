e5c963f68f75bdca6ab98a8b28adaf1527c86479
ostandrey <ost.and.g@gmail.com>
2023-08-06 14:15:15 +0300

    feat: create animations

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function handleBurger() {
   const menuButton = document.querySelector('.header__menu-burger');
   const menuMobile = document.querySelector('.header__menu-mobile');
   const menuWrapper = document.querySelector('.header__menu-wrapper');
-  const navLinks = document.querySelectorAll('.header__menu-link');
+  const navLinks = document.querySelectorAll('.header__menu-mobile-link');
   navLinks.forEach((link) => link.addEventListener('click', toggleMenu));
 
   menuButton.addEventListener('click', toggleMenu);
